03d6a37b3dcae90597665cd29e6003324ecfafb5
huangxiaoming <smile.pasta@yahoo.com>
2017-07-13 08:29:46 +0800

    modified  directory structure

--- ctrpto.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const hash = crypto.createHash('md5');
-
-hash.update('Hello World!');
-hash.update('Hello world2');
-
-console.log(hash.digest('hex'))
--- stream.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-var rs = fs.createReadStream('main.js');
-var ws = fs.createWriteStream('copy_main.txt');
-
-rs.pipe(ws)
