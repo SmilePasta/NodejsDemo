8b368ee5df9daf570ff9c5106737a883655830cb
huangxiaoming <smile.pasta@yahoo.com>
2017-07-14 11:52:32 +0800

    提交时忽略node_modules文件夹

--- db_demo/models/user.js
+++ db_demo/models/user.js
@@ -1,8 +1,8 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    username: DataTypes.STRING
-  });
-  return User;
+    var User = sequelize.define("User", {
+        username: DataTypes.STRING
+    });
+    return User;
 };
